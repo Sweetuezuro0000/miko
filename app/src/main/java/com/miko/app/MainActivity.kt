fe1aee9c77f6d07fa81988dc230f915672c4ddf3
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-07-06 19:39:34 +0530

    Add MainActivity with HomeScreen setup

---
+++ app/src/main/java/com/miko/app/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.miko.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.miko.app.ui.HomeScreen
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            MaterialTheme {
+
+                Surface {
+
+                    HomeScreen()
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
