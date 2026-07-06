046bfa4225669212a22769f8613585a4871422d3
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-07-06 20:08:00 +0530

    Create Note.kt

---
+++ app/src/main/java/com/miko/app/data/note/Note.kt
@@ -0,0 +1,18 @@
+package com.miko.app.data.note
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class Note(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val title: String,
+
+    val content: String,
+
+    val createdAt: Long = System.currentTimeMillis()
+
+)
