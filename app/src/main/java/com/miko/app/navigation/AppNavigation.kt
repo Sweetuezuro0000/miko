a45c243007fc8ab53bfcedf601c282d090eefda3
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-07-06 20:46:58 +0530

    Add AppNavigation composable for navigation setup

---
+++ app/src/main/java/com/miko/app/navigation/AppNavigation.kt
@@ -0,0 +1,20 @@
+package com.miko.app.navigation
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import com.miko.app.ui.HomeScreen
+
+@Composable
+fun AppNavigation(
+
+    context: Context
+
+) {
+
+    HomeScreen(
+
+        context
+
+    )
+
+}
