ec873fcf02c1990865c90d429113589dfd17315c
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-07-07 19:25:54 +0530

    Update MainActivity.kt

--- app/src/main/java/com/miko/app/MainActivity.kt
+++ app/src/main/java/com/miko/app/MainActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.miko.app.ui.HomeScreen
+import com.miko.app.utils.PermissionManager
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
-        setContent {
-
-            HomeScreen(this)
-
+        // Runtime permissions
+        if (!PermissionManager.hasPermissions(this)) {
+            PermissionManager.request(this)
         }
 
+        setContent {
+            HomeScreen(this)
+        }
     }
-
 }
