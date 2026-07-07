a741cd45a12d0bb49cd86289736963f1fccbea18
Sweetuezuro0000 <113238512+Sweetuezuro0000@users.noreply.github.com>
2026-07-07 18:28:47 +0530

    Create Command.kt

---
+++ app/src/main/java/com/miko/app/ai/Command.kt
@@ -0,0 +1,23 @@
+package com.miko.app.ai
+
+enum class Command {
+
+    NOTE,
+
+    DOCUMENT,
+
+    REMINDER,
+
+    SETTINGS,
+
+    SEARCH,
+
+    FLASHLIGHT_ON,
+
+    FLASHLIGHT_OFF,
+
+    TIME,
+
+    UNKNOWN
+
+}
